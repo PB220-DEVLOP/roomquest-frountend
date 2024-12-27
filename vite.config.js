8be6b2bcba38feae5b59adc9be88ae32e480222f
PB220-DEVLOP <pratikbhoyar2002@gmail.com>
2024-12-27 15:09:54 +0530

    basic rout error solved

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
+  server: {
+    historyApiFallback: true, // Ensure it works in dev server too
+  },
   build: {
-    outDir: 'dist', // Ensure this matches your intended output directory
+    outDir: 'dist',
   },
 });
