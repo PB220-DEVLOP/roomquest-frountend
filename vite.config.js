a5440af9f4b7bd0501d6767f970a4224420cd5a8
PB220-DEVLOP <pratikbhoyar2002@gmail.com>
2024-12-27 14:21:10 +0530

    basic commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   build: {
-    outDir: 'frontend/dist',  // This ensures the build files go into the dist folder
-    chunkSizeWarningLimit: 1000, // Set the chunk size limit to 1000 KB
+    outDir: 'dist', // Ensure this matches your intended output directory
   },
-})
+});
